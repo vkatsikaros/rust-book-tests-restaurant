f7cf47cdee757cbc8c49bd56b399290397eda0f0
Vangelis Katsikaros <vkatsikaros@gmail.com>
2022-03-16 10:37:01 +0200

    Add a module

--- src/lib.rs
+++ src/lib.rs
@@ -6,3 +6,18 @@ mod tests {
         assert_eq!(result, 4);
     }
 }
+
+
+mod front_of_house {
+    mod hosting {
+        fn add_to_waitlist() {}
+
+        fn seat_at_table() {}
+    }
+
+    mod serving {
+        fn take_order() {}
+
+        fn serve_order() {}
+    }
+}
